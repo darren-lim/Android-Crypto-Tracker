b0523008939d86a8128fca1fd42a9e1a253f6bbf
Darren <lim.darren.j@gmail.com>
2022-07-27 22:40:39 -0700

    structure setup

--- app/src/main/java/com/example/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/example/cryptotracker/MainActivity.kt
@@ -19,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.bottomNavigationView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(R.id.bottomNavigationView)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
